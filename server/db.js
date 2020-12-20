247fc1e79e20903f69ddf19db536fcb03c4d6346
odelia <otropper@gmail.com>
2020-12-20 20:23:20 +0200

    added DB connection to atlas

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-const dbUrl = 'mongodb://localhost:27017';
-const dbName = 'todo-app';
+const dbUrl = 'mongodb://localhost:27017/todo-app';
+if (process.env.DB_URL) {
+    dbUrl = process.env.DB_URL;
+}
 
 async function connect() {
-    await mongoose.connect(`${dbUrl}/${dbName}`, {
+    await mongoose.connect(`${dbUrl}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
